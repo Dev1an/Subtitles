190271a3b428ab608b0bef0082b91dea233facbc
Devian <info@devian.tk>
2015-05-07 09:32:29 +0200

    correct reordering

--- Router.js
+++ Router.js
@@ -6,7 +6,7 @@ Router.route('/editor/:document', function () {
   this.render('editor', {
   	data: function() {
   		return {
-  			titles: Titles.find({document: this.params.document}),
+  			titles: Titles.find({document: this.params.document}, {sort: {order: 1}}),
   			document: Documents.findOne(this.params.document)
   		}
   	}
